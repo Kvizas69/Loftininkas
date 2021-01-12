dc0ba841ad74fc527cf2d5a387e8124de3eba2ef
Kvizas69 <77357466+Kvizas69@users.noreply.github.com>
2021-01-12 21:22:08 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,15 +1,21 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
-const prefix = '+'
+const prefix = '-';
 
-const token = '';
 
 client.once('ready', () => {
   console.log('Loftinikas prisijunges');
 })
-client.on('message', message =>{ //kaip botas reguoja i zinute
 
+client.on('message', message =>{ //kaip botas reguoja i zinute
+  if(!message.content.startsWith(prefix) || message.author.bot) return; //nereguos jeigu nera prefixo
+  
+  const args = message.content.slice(prefix.length).split(" ");
+  const command = args.shift().toLowerCase();
+  
+  if(command == 'salam'){
+    message.channel.send('aleikum!');
+  }
 });
 
 client.login(token);
-
